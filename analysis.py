4c6f15ee877250b6055842c7d83c5c637350960a
monatagelsir7 <monatagelsir777@gmail.com>
2024-09-10 14:45:26 -0500

    adding some comments

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
 columns = ['Mortality rate, infant (per 1,000 live births)', 'GDP per capita (constant 2010 US$)', 'Country Name']
 df = data[columns]
+# GDP Per Capita has a REALLY long right tail, so we want to log it for readability.
 data["Log GDP Per Capita"] = np.log(data["GDP per capita (constant 2010 US$)"])
 import seaborn.objects as so
 import seaborn as sns
